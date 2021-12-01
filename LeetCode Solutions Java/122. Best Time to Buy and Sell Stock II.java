2dc8e60144bd5e84ec0b5ed0d3d4d049d4e828be
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2021-12-02 03:00:28 +0530

    Added first commit

---
+++ LeetCode Solutions Java/122. Best Time to Buy and Sell Stock II.java
@@ -0,0 +1,12 @@
+class Solution {
+//time:O(n)
+    public int maxProfit(int[] prices) {
+        int sum = 0;
+        for(int i=1; i<prices.length; i++){
+            if(prices[i-1] < prices[i]){
+                sum += prices[i] - prices[i-1];
+            }
+        }
+        return sum;
+    }
+}
